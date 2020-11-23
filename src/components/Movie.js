10dfbfccb86bddef167d2c4b2165abef1f137478
Alex Yaro <alxyaro@gmail.com>
2020-11-22 19:27:54 -0500

    Fix averageRating star fill

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -6,7 +6,7 @@ export default class Movie extends React.Component {
 		return <div className="movie">
 			<div style={{backgroundImage: "url(https://m.media-amazon.com/images/M/MV5BMDU2ZWJlMjktMTRhMy00ZTA5LWEzNDgtYmNmZTEwZTViZWJkXkEyXkFqcGdeQXVyNDQ2OTk4MzI@._V1_SX300.jpg)"}}>
 				<div className="info">
-					<Rating movie={{averageRating: 4.5, userRating: 3}}/>
+					<Rating movie={{averageRating: 3.5, userRating: 3}}/>
 					<h2>Toy Story (1998)</h2>
 				</div>
 			</div>
