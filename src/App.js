55fd97d9a45a74861ea2ef8b68fbba5a127f9359
Alex Yaro <alxyaro@gmail.com>
2020-11-24 01:04:40 -0500

    Add scroll-based lazy loading of movies

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 //import logo from './logo.svg';
-import {Component} from "react";
+import React from "react";
 import Movie from "./components/Movie";
 
 import * as system from "./system";
 
-export default class App extends Component {
+export default class App extends React.Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
 			movies: system.nextBatch(12)
 		};
+
+		this.scrollRef = React.createRef();
+		let lastLoad = 0
+		window.addEventListener("scroll", e => {
+			let reachedPos = window.innerHeight;
+			if (reachedPos-150 > this.scrollRef.current.getBoundingClientRect().y) {
+				if (Date.now()-lastLoad > 100) {
+					lastLoad = Date.now();
+					this.state.movies.push(...system.nextBatch(12));
+					this.setState({
+						movies: this.state.movies
+					});
+				}
+			}
+		});
 	}
 
 	render() {
@@ -20,8 +35,9 @@ export default class App extends Component {
 					{/*<img src={logo} className="App-logo" alt="logo"/>*/}
 				</header>
 				<div className="movies-container">
-					{this.state.movies.map((movie, i) => <Movie key={movie.id} movie={movie} fadein={i*100}/>)}
+					{this.state.movies.map((movie, i) => <Movie key={movie.id} movie={movie} fadein={(i % 12)*100}/>)}
 				</div>
+				<div id="load-delimiter" ref={this.scrollRef}/>
 			</div>
 		);
 	}
