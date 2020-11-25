74f6609b90f5fc85f5a0c09d40460669adea2e1f
Ian MacPherson <ianmicmac@hotmail.com>
2020-11-25 12:20:25 -0500

    Add 'Recommended for you' section

--- src/App.js
+++ src/App.js
@@ -67,8 +67,19 @@ export default class App extends React.Component {
 					</div>
 					<hr />
 				</div>
-				<div className="movies-container">
-					{this.state.movies.map((movie, i) => <Movie key={movie.id} movie={movie} fadein={(i % 12)*100} addToList={this.addToMyList} />)}
+				<div>
+					<h3>Recommended for You</h3>
+					<hr/>
+						TO DO
+					<hr/>
+				</div>
+				<div>
+					<h3>Movies</h3>
+					<hr/>
+					<div className="movies-container">
+						{this.state.movies.map((movie, i) => <Movie key={movie.id} movie={movie} fadein={(i % 12)*100} addToList={this.addToMyList} />)}
+					</div>
+					<hr/>
 				</div>
 				<div id="load-delimiter" ref={this.scrollRef}/>
 			</div>
