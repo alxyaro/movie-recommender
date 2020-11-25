5fab7985aa700dfa2f334f2ed94829fae3efeef9
Ian MacPherson <ianmicmac@hotmail.com>
2020-11-25 14:45:32 -0500

    filter common movies with current user and each user

--- src/system.js
+++ src/system.js
@@ -24,18 +24,41 @@ export function ratingUpdated(movie) {
 		currentUserRatings[index] = movie;
 		localStorage.setItem('userRatings', JSON.stringify(currentUserRatings))
 	}
-	
+
+	calculateSimilarity()
 }
 
-export function calculateSimilarity(userRatings) {
+export function calculateSimilarity() {
+	let userRatings = localStorage.getItem('userRatings') ? JSON.parse(localStorage.getItem('userRatings')) : []
 	if (!userRatings)
 		throw new Error("The current user has not rated any movies");
-	// Array of movie objects with user's rating will be passed
+	
+		// Array of movie objects with user's rating will be passed
 	// Use user's ratings to calculate similarity scores with other 
 	// Users and make recommendations
-	var ratingsArray = Object.values(ratings[2])
-	console.log(ratingsArray)
-	console.log(userRatings)
+
+	let currUserMoviesRated = userRatings.map(e => e.id)
+	
+	// Average rating for current user
+	var avgCurrUserMoviesRated = userRatings.reduce(function(acc, movie) {
+		return acc + movie.userRating
+	},0) / userRatings.length
+
+	let usersArray = Object.keys(ratings)
+	let usersRatingsArray = Object.values(ratings)
+	console.log("Array of user ID's:", usersArray)
+	console.log("Array of user Ratings:", usersRatingsArray)
+	var moviesRated = [];
+
+
+	var i;
+	for (i = 0; i < usersArray.length; i++) {
+		// For each user and their ratings, get intersection with current user
+		moviesRated = Object.keys(usersRatingsArray[i])
+		// Common movies will be an intersection of movies that both users have rated in common
+		let commonMovies = moviesRated.filter((e) => currUserMoviesRated.indexOf(e) !== -1)
+		
+	}
 }
 
 export function nextBatch(size) {
