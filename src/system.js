ad79fc5636be983096a415b173e2c7485cb8133a
Alex Yaro <alxyaro@gmail.com>
2020-12-01 18:05:39 -0500

    Add extra documentation for new system logic

--- src/system.js
+++ src/system.js
@@ -1,5 +1,5 @@
 const movies = require("./content/movies.json");
-const ratings = require("./content/ratings.json");
+const ratingsMap = require("./content/ratings.json");
 
 // INITIALIZATION LOGIC
 const movieIdsToMovies = {};
@@ -7,8 +7,8 @@ for (let movie of movies)
 	movieIdsToMovies[movie.id] = movie;
 
 const avgUserRatingMap = {};
-for (let userId in ratings) {
-	const ratedMovies = Object.values(ratings[userId]);
+for (let userId in ratingsMap) {
+	const ratedMovies = Object.values(ratingsMap[userId]);
 	if (ratedMovies.length === 0) // shouldn't be the case, but just in case
 		continue;
 	avgUserRatingMap[userId] = ratedMovies.reduce((acc, rating) => acc+rating) / ratedMovies.length;
@@ -34,7 +34,7 @@ function calcPearsonCorrelationCoefficient(obj1, avg1, obj2, avg2) {
 	}
 
 	// the following 3 variables are for the pearson correlation coefficient math
-	let numeratorSum = 0;
+	let numerator = 0;
 	let denominatorSum1 = 0;
 	let denominatorSum2 = 0;
 
@@ -45,7 +45,7 @@ function calcPearsonCorrelationCoefficient(obj1, avg1, obj2, avg2) {
 			const rating1 = obj1[movieId]-avg1;
 			const rating2 = obj2[movieId]-avg2;
 
-			numeratorSum += rating1*rating2;
+			numerator += rating1*rating2;
 			denominatorSum1 += Math.pow(rating1, 2);
 			denominatorSum2 += Math.pow(rating2, 2);
 
@@ -55,66 +55,70 @@ function calcPearsonCorrelationCoefficient(obj1, avg1, obj2, avg2) {
 		}
 	}
 
-	if (denominatorSum1 === 0 || denominatorSum2 === 0) return 0;
-	const result = numeratorSum/Math.sqrt(denominatorSum1*denominatorSum2);
-	return Math.min(result, 1); // sometimes, the result can have precision error & be slightly larger than 1
+	const denominator = Math.sqrt(denominatorSum1*denominatorSum2);
+	if (denominator === 0) return 0;
+	const result = numerator/denominator;
+	return Math.min(result, 1); // sometimes, the result can have precision error & be slightly larger than 1; so cap it
 }
 
 function calcEstimatedRating(similarities, movieId) {
 	// this is done using weighted-sum aggregation
-	let numeratorSum = 0;
-	let denominatorSum = 0;
+	let numerator = 0;
+	let denominator = 0;
 	for (let simObj of similarities) {
 		const userId = simObj.userId;
-		const ratingData = ratings[userId];
+		const ratingData = ratingsMap[userId];
 		if (ratingData.hasOwnProperty(movieId)) {
-			numeratorSum += simObj.sim * ratingData[movieId];
-			denominatorSum += simObj.sim;
+			numerator += simObj.sim * ratingData[movieId];
+			denominator += Math.abs(simObj.sim); // abs value for good measure & formula formality; negative similarities are discarded
 		}
 	}
-	return denominatorSum > 0 ? numeratorSum/denominatorSum : 0;
+	return denominator > 0 ? numerator/denominator : 0;
 }
 
 function calcRecommendedMovies() {
-	// calculate similarities using pearson correlation coefficient
+	// calculate recommendations using pearson correlation coefficient similarity + weighted-sum aggregation estimates
 	const userRatings = Object.values(userRatingsObj);
 	if (userRatings.length === 0)
-		return [];
+		return []; // no ratings; nothing to compare to
 
 	let avgUserRating = userRatings.reduce((acc, rating) => acc+rating) / userRatings.length;
 
 	let roundedAvgUserRating = Math.round(avgUserRating);
 	if (userRatings.every(rating => rating === roundedAvgUserRating))
-		return []; // no enough rating variety (all ratings are the same)
+		return []; // not enough rating variety (all ratings are the same)
 
 	let similarities = [];
 
-	for (let userId in ratings) {
-		const altUserRatingsObj = ratings[userId];
+	for (let userId in ratingsMap) {
+		const altUserRatingsObj = ratingsMap[userId];
 		let avgAltUserRating = avgUserRatingMap[userId];
-		if (!avgAltUserRating)
+		if (!avgAltUserRating) // just in case; all users should have at least one ratings, thus have an average rating
 			continue;
 
 		const similarity = calcPearsonCorrelationCoefficient(userRatingsObj, avgUserRating, altUserRatingsObj, avgAltUserRating);
-		if (similarity > 0)
+		if (similarity > 0) // only consider positive similarities
 			similarities.push({userId: userId, sim: similarity});
 	}
 
-	similarities.sort((a, b) => b.sim-a.sim); // sort by desc similarities
+	similarities.sort((a, b) => b.sim-a.sim); // sort similarities by desc order (most similar go first)
 	// console.log("similarities:", similarities);
-	similarities = similarities.slice(0, 20); // only use top 20 similarities
+	similarities = similarities.slice(0, 20); // only use the top 20 similarities
 
 	const result = [];
 	for (let movie of movies) {
 		delete movie.match;
 		if (!movie.userRating) {
 			result.push(movie);
-			movie.match = calcEstimatedRating(similarities, movie.id) / 5;
+			// calculate the estimated rating for this movie, averaging the ratings of similar users
+			movie.match = calcEstimatedRating(similarities, movie.id);
+			// then, map this from [1, 5] to [0, 1], so it's a "recommendation match percentage"
+			movie.match = (calcEstimatedRating(similarities, movie.id)-1) / 4;
 		}
 	}
-	result.sort((a, b) => b.match-a.match);
+	result.sort((a, b) => b.match-a.match); // sort movies by descending matches (highest matched movies go first)
 	// console.log("scores:", result.map(movie => movie.match));
-	return result;
+	return result; // enjoy
 }
 
 export function getAllMovies() {
